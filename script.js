5e4fc73f2fa720e75d445a9f99fee7561cb779b8
MariyammalK <mari892002@gmail.com>
2025-10-27 20:51:22 +0530

    Add files via upload
    this is my portfolio

---
+++ script.js
@@ -0,0 +1,16 @@
+// Mobile menu toggle
+const menuIcon = document.getElementById("menu-icon");
+const navLinks = document.querySelector(".nav-links");
+
+menuIcon.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+// Scroll Animations
+ScrollReveal().reveal(".hero-text", { delay: 200, origin: "bottom", distance: "50px", duration: 1000 });
+ScrollReveal().reveal(".about-text", { delay: 300, origin: "left", distance: "60px", duration: 1000 });
+ScrollReveal().reveal(".about-img", { delay: 300, origin: "right", distance: "60px", duration: 1000 });
+ScrollReveal().reveal(".project-card", { delay: 200, origin: "bottom", distance: "50px", interval: 200 });
+ScrollReveal().reveal("form", { delay: 300, origin: "bottom", distance: "50px", duration: 1000 });
+
+
